96f4ba41e953c2f760858e15a34726b62d1dad20
studentstestaccount <125878401+studentstestaccount@users.noreply.github.com>
2023-02-20 13:14:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-def are_matching(left, right):
-    matching_pairs = {"(": ")", "[": "]", "{": "}"}
-    return left in matching_pairs and right == matching_pairs[left]
 
-def find_mismatch(text):
-    stack = []
-    for i, c in enumerate(text, 1):
-        if c in '([{':
-            stack.append((c, i))
-        elif c in ')]}':
-            if not stack or not are_matching(stack.pop()[0], c):
-                return i
-    return 'Success' if not stack else stack[0][1]
-
-if 'I' in input():
-    print(find_mismatch(input()))
