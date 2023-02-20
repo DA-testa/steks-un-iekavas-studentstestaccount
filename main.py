a04c34a2298781412f50f8dd9b6070410acd157f
studentstestaccount <125878401+studentstestaccount@users.noreply.github.com>
2023-02-20 13:25:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,53 @@
-
-from collections import namedtuple
-
-Bracket = namedtuple("Bracket", ["char", "position"])
+class Bracket:
+    def __init__(self, char, position):
+        self.char = char
+        self.position = position
 
 
 def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
+    matching_pairs = {"(": ")", "[": "]", "{": "}"}
+    return left in matching_pairs and right == matching_pairs[left]
 
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next_char in enumerate(text, 1):
+    for i, next_char in enumerate(text):
         if next_char in "([{":
             opening_brackets_stack.append(Bracket(next_char, i))
         elif next_char in ")]}":
             if not opening_brackets_stack:
-                return i
-            top = opening_brackets_stack.pop()
+                return i, "Unmatched closing bracket: {}".format(next_char)
+            top = opening_brackets_stack[-1]
             if not are_matching(top.char, next_char):
-                return i
+                return i, "Expected {}, but found {}".format(
+                    matching_pairs[top.char], next_char
+                )
+            opening_brackets_stack.pop()
     if opening_brackets_stack:
-        return opening_brackets_stack[0].position
-    return "Success"
+        top = opening_brackets_stack[-1]
+        return top.position, "Unmatched opening bracket: {}".format(top.char)
+    return -1, "Success"
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    if mismatch == "Success":
-        print(mismatch)
-    else:
-        print(mismatch)
+    while True:
+        try:
+            lines = []
+            while True:
+                line = input()
+                if line == "I'm done":
+                    break
+                lines.append(line)
+            text = "".join(lines)
+            mismatch, error_message = find_mismatch(text)
+            if mismatch == -1:
+                print("No mismatches found")
+            else:
+                print("Mismatch at position {}: {}".format(mismatch + 1, error_message))
+        except (EOFError, KeyboardInterrupt):
+            print("\nExiting program")
+            break
 
 
-if _name_ == "_main_":
-    main()
+if __name__ == "__main__":
+    main()
